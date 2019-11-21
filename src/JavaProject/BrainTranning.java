fc7f57b17abfd123d328f8960c388f08292d5794
Admin <Admin@Admin>
2019-11-21 23:14:55 +0700

    Update_ Huy

---
+++ src/JavaProject/BrainTranning.java
@@ -0,0 +1,250 @@
+package JavaProject;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.Timer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class BrainTranning extends javax.swing.JFrame {
+    public BrainTranning() {
+        initComponents();
+    }
+    private int score=0,a,timeEasy=20;
+    private Timer timer;
+ 
+    public void random(){
+     Random rd= new Random();  
+        if(score>=0&&score<=2){   
+           a= rd.nextInt(999)+1;
+           numran.setText(String.valueOf(a));
+        }
+        else if(score>2&&score<=4){
+           a= rd.nextInt(99+9999)+1;
+           numran.setText(String.valueOf(a));
+        }   
+        else if(score>4&&score<=7){
+         a= rd.nextInt(9999+999999)+1;
+         numran.setText(String.valueOf(a));
+        }
+        else{
+         a= rd.nextInt(100000000)+1;
+         numran.setText(String.valueOf(a));
+        }   
+    }
+    public class loadTime implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(timeEasy>0){
+            timeEasy--;
+            timeOn.setText(String.valueOf(timeEasy));
+                if(timeEasy>=10){
+                note.setText("You have 10 seconds to memorize");
+                input.setText("");
+                }
+                if(timeEasy<=10){
+                numran.setText("Write your answer");
+                note.setText("You have 10 seconds to write");
+                if(timeEasy==0){
+                 
+                numran.setText("Your Score: "+score);
+                timer.stop();
+                status.setText("The number is "+a);  
+       }
+                }
+              
+            }                  
+            }
+        }
+    
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        timeOn = new javax.swing.JLabel();
+        numran = new javax.swing.JLabel();
+        note = new javax.swing.JLabel();
+        input = new javax.swing.JTextField();
+        ok = new javax.swing.JButton();
+        status = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Source Sans Pro Semibold", 2, 24)); // NOI18N
+        jLabel1.setText("Brain Tranning");
+
+        timeOn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        timeOn.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        timeOn.setText("TIME");
+
+        numran.setFont(new java.awt.Font("Sylfaen", 0, 36)); // NOI18N
+        numran.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        numran.setText("LOOK IN HERE");
+
+        note.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        note.setText("You have 10s to memorize and 10s to write");
+
+        input.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                inputKeyReleased(evt);
+            }
+        });
+
+        ok.setText("Start");
+        ok.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                okActionPerformed(evt);
+            }
+        });
+
+        status.setFont(new java.awt.Font("Sylfaen", 0, 24)); // NOI18N
+        status.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                            .addGap(37, 37, 37)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(input)
+                                .addGroup(jPanel1Layout.createSequentialGroup()
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGap(90, 90, 90)
+                                    .addComponent(timeOn, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(status, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(numran, javax.swing.GroupLayout.PREFERRED_SIZE, 317, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(note, javax.swing.GroupLayout.PREFERRED_SIZE, 317, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(166, 166, 166)
+                        .addComponent(ok, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(46, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(timeOn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(38, 38, 38)
+                .addComponent(numran, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(note, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(input, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(ok)
+                .addGap(28, 28, 28)
+                .addComponent(status, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void okActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okActionPerformed
+
+        timeEasy=20;
+        random();
+        timer= new Timer(1000, new loadTime());
+        timer.start();
+    }//GEN-LAST:event_okActionPerformed
+
+    private void inputKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_inputKeyReleased
+       
+       try{
+       int autoCheck=Integer.parseInt(input.getText());
+       if(autoCheck==a&&timeEasy<=10){     
+        score++;
+        timer.stop();
+        timeEasy=20;       
+        random();
+        timer.start();
+       }
+       }
+       catch(NumberFormatException er){
+        timer.stop();
+        note.setText("You must enter a number");
+        timer.start();
+        input.setText("");
+    }//GEN-LAST:event_inputKeyReleased
+    }
+   
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(BrainTranning.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(BrainTranning.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(BrainTranning.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(BrainTranning.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new BrainTranning().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField input;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel note;
+    private javax.swing.JLabel numran;
+    private javax.swing.JButton ok;
+    private javax.swing.JLabel status;
+    private javax.swing.JLabel timeOn;
+    // End of variables declaration//GEN-END:variables
+}
